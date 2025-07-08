358732f18ef78f602f3a8bf153301f79c8cbe0f4
mathew <mathew.m.daniel@gmail.com>
2025-07-08 06:10:39 +0200

    Commit v.1.6d - Category & Subcategory handling bug-fixes. 1. Changed passing of subcategory as id, not object.

--- app/Http/Controllers/Admin/SubCategoryController.php
+++ app/Http/Controllers/Admin/SubCategoryController.php
@@ -12,7 +12,7 @@ class SubCategoryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request, int $categoryId = null)
+    public function index(Request $request, int $categoryId)
     {
         if ($categoryId) {
             $subCategories = SubCategory::where('category_id', $categoryId)->get();
@@ -53,14 +53,22 @@ class SubCategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, SubCategory $subCategory)
+    public function update(Request $request, int $subCategoryId)
     {
         $request->validate([
             'name' => 'required|string|max:255',
             'category_id' => 'required|exists:categories,id'
         ]);
 
-        $subCategory->update($request->only('name', 'category_id'));
+        try {
+
+            $subCategory = SubCategory::findOrFail($subCategoryId);
+            $subCategory->update($request->only('name', 'category_id'));
+        }
+
+        catch (\Exception $ex) {
+            return back()->with('error', $ex->getMessage());
+        }
 
         return $request->wantsJson()
             ? $subCategory
@@ -70,8 +78,10 @@ class SubCategoryController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(SubCategory $subCategory)
+    public function destroy(int $subCategoryId)
     {
+        $subCategory = SubCategory::findOrFail($subCategoryId);
+
         $subCategory->delete();
 
         return request()->wantsJson()
